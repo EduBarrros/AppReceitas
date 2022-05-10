6127f8146963c3c86f9a1fc7a49933bb6a83c835
EduBarrros <74619318+EduBarrros@users.noreply.github.com>
2022-05-10 14:41:49 -0300

    feat: finish loginScreen

--- screens/Login.js
+++ screens/Login.js
@@ -7,7 +7,8 @@ import {
     StatusBar
 } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
-import { images, COLORS, SIZES, FONTS } from "../constants"
+import { images, COLORS, SIZES, FONTS } from "../constants";
+import { CustomButton } from '../components';
 
 const Login = ({ navigation }) => {
 
@@ -39,8 +40,68 @@ const Login = ({ navigation }) => {
                             paddingHorizontal: SIZES.padding
                         }}
                     >
+                        <Text
+                            style={{
+                                width: '80%',
+                                color: COLORS.white,
+                                ...FONTS.largeTitle,
+                                lineHeight: 45
+                            }}
+                        >
+                            Cooking a Delicious Food Easily
+                        </Text>
                     </LinearGradient>
                 </ImageBackground>
+            </View>
+        )
+    }
+
+    const renderDetail = () => {
+        return (
+            <View
+                style={{
+                    flex: 1,
+                    paddingHorizontal: SIZES.padding
+                }}
+            >
+                <Text
+                    style={{
+                        marginTop: SIZES.radius,
+                        width: '70%',
+                        color: COLORS.gray,
+                        ...FONTS.body3
+                    }}
+                >
+                    Discover more than 1200 recipes in your hands and cooking it easily!
+                </Text>
+                <View
+                    style={{
+                        flex: 1,
+                        justifyContent: 'center'
+                    }}
+                >
+                    <CustomButton
+                        buttonText={'Login'}
+                        buttonContainerStyle={{
+                            paddingVertical: 18,
+                            borderRadius: 20
+                        }}
+                        colors={[COLORS.darkGreen, COLORS.lime]}
+                        onPress={() => navigation.replace('Home')}
+                    />
+                    <CustomButton
+                        buttonText={'Sign Up'}
+                        buttonContainerStyle={{
+                            marginTop: SIZES.radius,
+                            paddingVertical: 18,
+                            borderRadius: 20,
+                            borderColor: COLORS.darkLime,
+                            borderWidth: 1
+                        }}
+                        colors={[]}
+                        onPress={() => navigation.replace('Home')}
+                    />
+                </View>
             </View>
         )
     }
@@ -54,6 +115,7 @@ const Login = ({ navigation }) => {
         >
             <StatusBar translucent backgroundColor="transparent" barStyle='light-content' />
             {renderHeader()}
+            {renderDetail()}
         </View>
     )
 }
