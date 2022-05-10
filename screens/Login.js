d4a332668a9fbed57daf5b2256bcc5c2dd82304d
EduBarrros <74619318+EduBarrros@users.noreply.github.com>
2022-05-10 12:51:34 -0300

    feat: bug linear gradient

--- screens/Login.js
+++ screens/Login.js
@@ -2,24 +2,56 @@ import React from 'react';
 import {
     View,
     Text,
-    TouchableOpacity
+    TouchableOpacity,
+    ImageBackground,
+    StatusBar
 } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import { images, COLORS, SIZES, FONTS } from "../constants"
 
 const Login = ({ navigation }) => {
+
+    const renderHeader = () => {
+        return (
+            <View
+                style={{
+                    height: SIZES.height > 700 ? '65%' : '60%'
+                }}
+            >
+                <ImageBackground
+                    source={images.loginBackground}
+                    style={{
+                        flex: 1,
+                        justifyContent: 'flex-end',
+                    }}
+                    resizeMode='cover'
+                />
+                <LinearGradient
+                    start={{ x: 0, y: 0 }}
+                    end={{ x: 0, y: 1 }}
+                    colors={[
+                        COLORS.transparent,
+                        COLORS.black
+                    ]}
+                    style={{
+                        height: 200,
+                        justifyContent: 'flex-end',
+                        paddingHorizontal: SIZES.padding
+                    }}
+                />
+            </View>
+        )
+    }
+
     return (
         <View
             style={{
                 flex: 1,
-                alignItems: 'center',
-                justifyContent: 'center'
+                backgroundColor: COLORS.black
             }}
         >
-            <Text>Login</Text>
-            <TouchableOpacity
-                onPress={() => navigation.replace("Home")}
-            >
-                <Text>Navigate to Home</Text>
-            </TouchableOpacity>
+            <StatusBar translucent backgroundColor="transparent" barStyle='light-content' />
+            {renderHeader()}
         </View>
     )
 }
